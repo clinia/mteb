451553c90bf8bf7b28dd9e62f1f59a7939af18cd
Daniel Buades Marcos <daniel.buades@clinia.com>
2024-10-01 13:29:35 -0400

    feat: add relevance judgments datasets

--- mteb/tasks/Retrieval/__init__.py
+++ mteb/tasks/Retrieval/__init__.py
@@ -104,7 +104,9 @@ from .kor.KoStrategyQA import *
 from .multilingual.BelebeleRetrieval import *
 from .multilingual.CrossLingualSemanticDiscriminationWMT19 import *
 from .multilingual.CrossLingualSemanticDiscriminationWMT21 import *
+from .multilingual.heMteb.DentistryAndOralHealthBinaryRelevanceJudgmentRetrieval import *
 from .multilingual.heMteb.DentistryAndOralHealthRetrieval import *
+from .multilingual.heMteb.DentistryAndOralHealthTertiaryRelevanceJudgmentRetrieval import *
 from .multilingual.heMteb.DermatologyRetrieval import *
 from .multilingual.heMteb.GastroenterologyRetrieval import *
 from .multilingual.heMteb.GeneticsRetrieval import *
