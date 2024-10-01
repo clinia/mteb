d7c0a09454c70c385ed29cb61a5c4d79e0e69f59
Daniel Buades Marcos <daniel.buades@clinia.com>
2024-10-01 13:03:33 -0400

    feat: add heMTEB tasks
    ---------
    
    Co-authored-by: Daniel Buades Marcos <daniel@buad.es>
    Co-authored-by: nadshe <nadia.sheikh@clinia.com>
    Co-authored-by: olivierr42 <olivier.rousseau@clinia.com>

---
+++ mteb/tasks/Retrieval/multilingual/heMteb/PlasticSurgeryRetrieval.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from mteb.abstasks.TaskMetadata import TaskMetadata
+
+from .....abstasks.AbsTaskRetrieval import AbsTaskRetrieval
+from .....abstasks.MultilingualTask import MultilingualTask
+from . import HeMtebTask
+
+_LANGUAGES = {
+    "en-en": ["eng-Latn", "eng-Latn"],
+    "es-en": ["spa-Latn", "eng-Latn"],
+    "fr-en": ["fra-Latn", "eng-Latn"],
+}
+
+
+class HeMtebPlasticSurgeryRetrieval(AbsTaskRetrieval, HeMtebTask, MultilingualTask):
+    metadata = TaskMetadata(
+        dataset={
+            "path": "clinia/hemteb-plastic-surgery-bm25",
+            "revision": "a96b70afa071d7e4913ea94d0b62a518b128f963",
+        },
+        name="HeMtebPlasticSurgery",
+        description="",
+        type="Retrieval",
+        modalities=["text"],
+        category="s2p",
+        reference=None,
+        eval_splits=["test"],
+        eval_langs=_LANGUAGES,
+        main_score="ndcg_at_10",
+        date=None,
+        domains=["Medical"],
+        task_subtypes=None,
+        license=None,
+        annotations_creators="expert-annotated",
+        dialect=None,
+        sample_creation=None,
+        bibtex_citation=None,
+        descriptive_stats={},
+    )
+
+    def load_data(self, **kwargs):
+        HeMtebTask.load_data(self, **kwargs)
