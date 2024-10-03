dc21564f4d9439e05f971a38615be6e2a2d3cb51
Brando Tovar <brando.tovar@clinia.com>
2024-10-03 14:26:41 -0400

    feat: passes encode kwargs to speed task

--- tests/test_tasks/test_metadata.py
+++ tests/test_tasks/test_metadata.py
@@ -8,6 +8,7 @@ from tests.test_benchmark.task_grid import MOCK_TASK_TEST_GRID
 @pytest.mark.parametrize("task", MOCK_TASK_TEST_GRID)
 def test_descriptive_stats(task):
     result_stat = task.calculate_metadata_metrics()
+    print(result_stat)
     task_stat = task.metadata.descriptive_stats
     for key, value in result_stat.items():
         assert key in task_stat
